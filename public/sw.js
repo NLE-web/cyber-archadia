3ed8aa81c16e6c55ce22b9d8502daf17b8ed2b83
nle <nicolas.lapiere@hotmail.com>
2026-05-05 14:57:25 +0200

    PWA

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'archadia-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // On ne met en cache que les requêtes GET
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
